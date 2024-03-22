299b5990a49e7b29a9488b68a7824601c4360b9a
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-22 22:43:15 +0000

    Task 1 bug: maybe a typo error?

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# Install a Python package using a Puppet manifest
+# install a Python package using a Puppet manifest
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => pip3,
   name     => 'flask',
 }
