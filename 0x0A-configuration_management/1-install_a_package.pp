583462031e3806034376f44e8e68c198f62cba9c
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-22 22:23:17 +0000

    Task 1 bug: Not sure what went wrong yet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install a Python package using a Puppet manifest
 
-package { 'installFlaskPackage':
+package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3',
+  name     => 'flask',
 }
