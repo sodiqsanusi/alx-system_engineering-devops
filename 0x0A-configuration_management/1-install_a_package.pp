24c3ab9d2e4babc3c32a1eecf56e49f588663dc6
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-23 13:32:32 +0000

    Task 1 bug: Apparently, I was supposed to install an additional package as well lol

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # Using Puppet, install flask from pip3.
 
-package { 'flask':
+package { 'installFlask':
       ensure   => '2.1.0',
       provider => 'pip3',
       name     => 'flask',
+}
+
+package { 'installWerkzeug':
+      ensure   => '2.1.1',
+      provider => 'pip3',
+      name     => 'Werkzeug',
 }
