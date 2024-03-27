1661d75d72574ef0f943386e6f66b34700a83990
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-26 17:07:06 -0700

    Task 7 (advanced): Wasn't able to run lint checks. 1% hope, 99% chance

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,33 @@
+# Configure an Nginx server. Also make some redirects
+
+package { 'installNginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+exec { 'enableFirewall':
+  command => 'sudo ufw enable',
+}
+
+exec { 'listenToHttpPort':
+  command => 'sudo ufw allow "Nginx HTTP"',
+}
+
+file { 'setRootPage':
+  ensure  => present,
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+}
+
+file_line { 'redirectionSetup':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  after  => 'listen 80 default_server;',
+}
+
+service { 'startNginx':
+  ensure  => running,
+  name    => 'nginx',
+  require => Package['nginx'],
+}
