09a4311e7d664e6b9411c0b7a304dbc81d7426ec
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-26 17:10:37 -0700

    Task 7 bug: Random acts of salvation at this point

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,25 @@
 # Configure an Nginx server. Also make some redirects
 
 package { 'installNginx':
-  ensure => installed,
+  ensure => 'installed',
   name   => 'nginx',
 }
 
 file { 'setRootPage':
-  ensure  => present,
+  ensure  => 'present',
   path    => '/var/www/html/index.nginx-debian.html',
   content => 'Hello World!',
 }
 
 file_line { 'redirectionSetup':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   after  => 'listen 80 default_server;',
 }
 
 service { 'startNginx':
-  ensure  => running,
+  ensure  => 'running',
   name    => 'nginx',
   require => Package['nginx'],
 }
