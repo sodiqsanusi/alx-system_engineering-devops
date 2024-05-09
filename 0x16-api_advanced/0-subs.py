7c173a01f4394e05beca8489148ea027f2e613c7
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-05-08 17:36:53 -0700

    Task 0 bug: still trying it out

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,31 +2,24 @@
 """
 A module containing functions for working with the Reddit API.
 """
+
+
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-
-
-def number_of_subscribers(sub_name):
+def number_of_subscribers(subreddit):
     """
-    Retrieves the number of subscribers in a given subreddit.
+    Retrieves the number of subscribers for a given subreddit.
     """
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, sub_name),
-        headers=api_headers,
-        allow_redirects=False
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        allow_redirects=False,
+        headers={"user-agent": "nabuntu_bot-01"},
+        timeout=60,
     )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    return 0
+
+    return (
+        response.json()["data"]["subscribers"]
+        if response.status_code == 200
+        else 0
+    )
